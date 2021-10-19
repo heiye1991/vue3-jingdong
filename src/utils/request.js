d1cc08c20528689746ef0c637bfac64f02eef243
heiye1991 <1604043458@qq.com>
2021-10-19 17:14:03 +0800

    首页获取附近店铺

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,30 @@
 import axios from 'axios'
 
-axios.defaults.baseURL =
-  'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/'
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+const instance = axios.create({
+  baseURL:
+    'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/',
+  timeout: 5000,
+})
+
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .get(url, params, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
-    axios
+    instance
       .post(url, data, {
         headers: {
           'Content-Type': 'application/json',
--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -1,52 +1,67 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div v-for="item in nearbyList" :key="item.id" class="nearby__item">
-      <img
-        class="nearby__item__img"
-        :src="`http://www.dell-lee.com/imgs/vue3/${item.img}.png`"
-        alt="nearby"
-      />
+    <div v-for="item in nearbyList" :key="item._id" class="nearby__item">
+      <img class="nearby__item__img" :src="item.imgUrl" alt="nearby" />
       <div class="nearby__item__content">
-        <div class="nearby__item__content__title">{{ item.title }}</div>
+        <div class="nearby__item__content__title">{{ item.name }}</div>
         <div class="nearby__item__content__tags">
-          <span
-            v-for="tag in item.tags"
-            :key="tag"
-            class="nearby__item__content__tags__tag"
-            >{{ tag }}
+          <span class="nearby__item__content__tags__tag">
+            月售：{{ item.sales }}
+          </span>
+          <span class="nearby__item__content__tags__tag">
+            起送：{{ item.expressLimit }}
+          </span>
+          <span class="nearby__item__content__tags__tag">
+            基础运费：{{ item.expressPrice }}
           </span>
         </div>
         <p class="nearby__item__content__highlight">
-          {{ item.desc }}
+          {{ item.slogan }}
         </p>
       </div>
     </div>
   </div>
+  <toast v-if="toastData.showToast" :message="toastData.toastMessage" />
 </template>
 
 <script>
+import { ref } from 'vue'
+import { get } from '@/utils/request'
+import Toast, { useToastEffect } from '@/components/Toast'
+
+const useNearbyListEffect = toastHandler => {
+  let nearbyList = ref([])
+  const getNearbyList = async () => {
+    try {
+      const result = await get('/api/shop/hot-list')
+      if (result?.errno === 0 && result?.data.length) {
+        nearbyList.value = result.data
+      } else {
+        toastHandler('登录失败')
+      }
+    } catch (err) {
+      toastHandler('请求失败')
+    }
+  }
+
+  return {
+    nearbyList,
+    getNearbyList,
+  }
+}
 export default {
   name: 'Nearby',
+  components: {
+    Toast,
+  },
   setup() {
-    const nearbyList = [
-      {
-        id: 1,
-        title: '沃尔玛',
-        tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-        desc: 'VIP尊享满89元减4元运费券（每月3张）',
-        img: 'near',
-      },
-      {
-        id: 2,
-        title: '沃尔玛',
-        tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-        desc: 'VIP尊享满89元减4元运费券（每月3张）',
-        img: 'near',
-      },
-    ]
+    const { toastData, toastHandler } = useToastEffect()
+    const { nearbyList, getNearbyList } = useNearbyListEffect(toastHandler)
+    getNearbyList()
 
     return {
+      toastData,
       nearbyList,
     }
   },
